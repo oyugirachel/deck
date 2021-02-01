21734e92bbe6d14e6c2c8fa76484f642cba413d5
oyugirachel <oyugirachel20@gmail.com>
2021-02-01 13:01:14 +0300

    added the deck function

--- card.go
+++ card.go
@@ -161,7 +161,13 @@ func Filter(f func(card Card) bool) func([]Card) []Card{
 // Deck function that allows for the addition of new decks
 func Deck(n int) func([]Card) []Card{
 	return func(cards []Card) []Card{
-		
+		var ret []Card
+		for i :=0; i<n; i++{
+			ret = append(ret,cards...)
+
+		}
+
+		return ret
 	}
 }
 
